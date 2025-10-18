cf9274404fee81e0ea940aa8cd6f649c7d3851e4
diegomillandev <diegomillandev@gmail.com>
2025-10-18 18:58:42 -0500

    feat: add shadcn and utility functions; update styles and globals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="">Page</div>;
+  return (
+    <div className="">
+      <Button>Click me</Button>
+    </div>
+  );
 }
